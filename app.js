fc77fc06e73a8247948aa894f7dbd0c0c8478236
MINVY-779VKA49\Bengel <minderrx2@gmail.com>
2022-05-11 21:46:12 +0100

    loads player info

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const fs = require('fs')
+const path = require('path')
+
+const playerPath = path.join(__dirname, 'src/db/player.json')
+const player = JSON.parse(fs.readFileSync(playerPath, 'utf8'));
+
+console.log(player)
+
 require('./src/routes')
