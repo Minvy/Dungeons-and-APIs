d2dd46a888c7cfcc82ebb2e25ce77ab29769a627
MINVY-779VKA49\Bengel <minderrx2@gmail.com>
2022-05-12 23:03:24 +0100

    added service spawning and destroying

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const playerUtils = require('./src/utils/player')
+const serviceUtils = require('./src/utils/service')
+
+serviceUtils.spawn('testing')
+serviceUtils.destroy('testing')
 
 require('./src/routes')
