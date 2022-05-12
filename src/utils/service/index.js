ccbbffe63c8c55885785c6dedf3f815017c48f32
MINVY-779VKA49\Bengel <minderrx2@gmail.com>
2022-05-12 23:03:35 +0100

    initial commit

---
+++ src/utils/service/index.js
@@ -0,0 +1,41 @@
+const { execSync } = require("child_process");
+
+const serviceUtils = {}
+
+serviceUtils.spawn = (name) => {
+    const shellCmd = [
+        'cd services',
+        `mkdir service-${name}`,
+        `cd service-${name}`,
+        'npm init -y',
+        'npm install express',
+        'npm install body-parser',
+        `echo {} > data.json`,
+        `echo console.log('hello world') > app.js`
+    ].join(' && ')
+    _execute(shellCmd)
+}
+
+serviceUtils.destroy = (name) => {
+    const shellCmd = [
+        'cd services',
+        `rd /s /q ${name}`,
+    ].join(' && ')
+    _execute(shellCmd)
+}
+
+const _execute = (shellCmd) => {
+    execSync(shellCmd, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
+
+module.exports = serviceUtils
